d87f95996dd422088e0afcbd12f693d63ac62565
flatironschool <danliu277@gmail.com>
2020-04-08 11:17:20 -0400

    Update route

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
-export const API_ROOT = 'http://localhost:3000';
-// export const API_ROOT = 'https://2792f0b8.ngrok.io';
-export const API_WS_ROOT = 'ws://localhost:3000/cable';
-// export const API_WS_ROOT = 'wss://2792f0b8.ngrok.io/cable';
+// export const API_ROOT = 'http://localhost:3000';
+export const API_ROOT = 'https://5a7d8fb4.ngrok.io';
+// export const API_WS_ROOT = 'ws://localhost:3000/cable';
+export const API_WS_ROOT = 'wss://5a7d8fb4.ngrok.io/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
